5599c88cb2d54e63284c500c96158d9102650f18
Anshika Gupta <anshikagknp@gmail.com>
2026-08-24 20:58:13 +0530

    feat: added application level and error handling middleware

--- 07-express/index.js
+++ 07-express/index.js
@@ -8,6 +8,25 @@ app.use(express.json())
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+
+// ---------- Middleware 1 (Application level middleware) ----------
+const middleware1 =(req, res, next) => {
+    console.log("Middleware 1 Calling....");
+    req.userData = {uid: 1001, unm: "Rahul"};
+    next();   
+}
+
+// ----------- Middleware 2 (Application level middleware)  -----------
+const middleware2 =(req, res, next) => {
+    console.log("Middleware 2 Calling....");
+    req.userData.unm = req.userData.unm.toUpperCase();
+    // req.userData = {uid: 1001, unm: req.userData.unm.toUpperCase()};
+    next();   
+}
+
+app.use(middleware1, middleware2);
+
 const adminRoutes = require('./routes/adminRoutes');
 // http://localhost:8000/admin
 app.use("/admin", adminRoutes);
@@ -19,6 +38,7 @@ app.use("/user", userRoutes);
 // http://localhost:8000
 // Handler function
 app.get("/",(req, res)=>{
+    console.log(req.userData);
     res.send("Hellowwww Mrs Monika")    //to send data in string format - res.send()
 })
 
@@ -31,7 +51,9 @@ app.post("/",(req, res)=>{
     res.send("Response from Post....")
 })
 
-app.put("/",(req, res)=>{
+app.put("/",(req, res, next)=>{
+    const err = new Error("Something Went Wrong.....");
+    return next(err);
     res.send("Response from Put....");
 })
 
@@ -48,3 +70,14 @@ app.listen(PORT, HOST, (error)=>{
         console.log(`Server Running at http://${HOST}:${PORT}`);       
 })      
 
+// ------------ Error Handling Middleware (used at last)---------------
+const errorHandler = (err, req, res, next) => {
+    console.log("Error Handling Middleware.......");
+    res.status(500).json({
+        success: false,
+        message: err.message,
+        // err : err.stack
+        err : err,
+    });
+};
+app.use(errorHandler);
