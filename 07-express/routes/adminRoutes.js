ba5a2c458961466a1b2a9bd3abab0c3581da1579
Anshika Gupta <anshikagknp@gmail.com>
2026-08-24 20:59:40 +0530

    feat: added route level middleware

--- 07-express/routes/adminRoutes.js
+++ 07-express/routes/adminRoutes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { adminDefault, adminHome, adminAbout, adminGetUser, adminAddUser } = require("../controllers/adminControllers");
 
+// -------------- Middleware 3 (Route level middleware) -------------
+const middleware3 = (req, res, next) => {
+    console.log("Admin Route Middleware......");
+    next();
+}
+router.use(middleware3);
+
 // http://localhost:8000/admin/
 router.get("/", adminDefault);
 
