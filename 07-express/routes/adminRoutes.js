94a209fa32648bb97b7daca8f38c8f17e63c3b2d
Anshika Gupta <anshikagknp@gmail.com>
2026-08-26 15:17:12 +0530

    feat: imported and established route for adminShowUsers

--- 07-express/routes/adminRoutes.js
+++ 07-express/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { adminDefault, adminHome, adminAbout, adminGetUser, adminAddUser } = require("../controllers/adminControllers");
+const { adminDefault, adminHome, adminAbout, adminGetUser, adminAddUser, adminShowUsers } = require("../controllers/adminControllers");
 
 // -------------- Middleware 3 (Route level middleware) -------------
 const middleware3 = (req, res, next) => {
@@ -15,11 +15,16 @@ router.get("/", adminDefault);
 // http://localhost:8000/admin/home
 router.get("/home", adminHome);
 
+// http://localhost:8000/admin/about
 router.get("/about", adminAbout);
 
+// http://localhost:8000/admin/user
 router.get("/user", adminGetUser);
 
 // http://localhost:8000/admin/add
 router.post("/add", adminAddUser);
+
+// http://localhost:8000/admin/show
+router.get("/show", adminShowUsers)
 
 module.exports = router;
