725c3d2d93492bfdfce74d4dab6e8d97d76a5474
Anshika Gupta <anshikagknp@gmail.com>
2026-08-28 00:02:39 +0530

    feat: added get, delete and put routes for respective controllers

--- 07-express/routes/adminRoutes.js
+++ 07-express/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { adminDefault, adminHome, adminAbout, adminGetUser, adminAddUser, adminShowUsers } = require("../controllers/adminControllers");
+const { adminDefault, adminHome, adminAbout, adminGetUser, adminAddUser, adminShowUsers, adminFindUser, adminDeleteUser, adminUpdateUser } = require("../controllers/adminControllers");
 
 // -------------- Middleware 3 (Route level middleware) -------------
 const middleware3 = (req, res, next) => {
@@ -19,12 +19,20 @@ router.get("/home", adminHome);
 router.get("/about", adminAbout);
 
 // http://localhost:8000/admin/user
-router.get("/user", adminGetUser);
+// router.get("/user", adminGetUser);
 
 // http://localhost:8000/admin/add
 router.post("/add", adminAddUser);
 
 // http://localhost:8000/admin/show
 router.get("/show", adminShowUsers)
+
+// http://localhost:8000/admin/user/1
+router.get("/user/:id", adminFindUser)
+
+// http://localhost:8000/admin/user/1
+router.delete("/user/:id", adminDeleteUser)
+
+router.put("/user/:id", adminUpdateUser)
 
 module.exports = router;
