95c1da7d8c5563364b4c4b3fb83914ff9b76a049
Anshika Gupta <anshikagknp@gmail.com>
2026-09-12 15:43:48 +0530

    feat(express): add CORS, cookie-parser, JWT login, logout, /me route, and error handler; protect admin/user routes with auth + authorize middleware

--- 07-express/index.js
+++ 07-express/index.js
@@ -3,12 +3,31 @@ const express = require('express');     // to import a module
 const db = require('./db_conn');    // database connectivity import
 const app = express()   // calling of an express function returns an app
 
-app.use(express.json())
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const cors = require('cors');
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+// to receive form data in req.body
+app.use(express.json());
+
+const authenticate = require('./middlewares/AuthMiddleware');
+const authorize = require('./middlewares/AuthorizeMiddleware');
+const errorHandler = require("./middlewares/errorMiddleware");
+const UserModel = require("./models/userModel");
+const { sendError, sendSuccess } = require("./utils/responseHelpers");
+const STATUS_CODES = require("./constants/statusCodes");
+const MESSAGES = require("./constants/messages");
+
+const bcrypt = require('bcrypt');
+const JWT = require('jsonwebtoken');
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8000;
-
-app.use(express.json());
 
 // Hide Password
 const sanitizeUser = (user)=>{
@@ -34,24 +53,41 @@ const middleware2 =(req, res, next) => {
 
 app.use(middleware1, middleware2);
 
-const bcrypt = require('bcrypt');
-
 // Login
 app.post("/login", async(req, res, next) => {
     try
     {
-        const { emailId, pwd } = req.body;
-        if(!pwd || !emailId)
+        const { mailId, pwd } = req.body;
+        if(!pwd || !mailId)
             return sendError(res, STATUS_CODES.BAD_REQUEST, MESSAGES.AUTH.MISSING_VALUES);
-        const user = await UserModel.findOne({userEmail : emailId});
+        const user = await UserModel.findOne({userEmail : mailId});
         if(!user)
             return sendError(res, STATUS_CODES.NOT_FOUND, MESSAGES.USER.NOT_FOUND);
         const isMatched = await bcrypt.compare(pwd, user.userPwd);
         
         if(!isMatched)
             return sendError(res, STATUS_CODES.UNAUTHORIZED, MESSAGES.AUTH.INVALID_CREDENTIALS);
+
         // JWT Token
+        const token = JWT.sign(
+            {
+                id: user._id,
+                role: user.userRole
+            },
+            process.env.JWT_SECRET_KEY,
+            {
+                expiresIn: "1d"
+            }
+        );
+
         // HTTP Cookie
+        res.cookie("jwtToken", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax",
+            maxAge: 60 * 60 * 1000 * 24
+        });
+
         return sendSuccess(res, STATUS_CODES.OK, MESSAGES.AUTH.LOGIN_SUCCESS, sanitizeUser(user));
     }
     catch(err)
@@ -60,13 +96,43 @@ app.post("/login", async(req, res, next) => {
     }
 })
 
+// Logout
+app.post("/logout", async(req, res, next) => {
+    try{
+        res.clearCookie("jwtToken", {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax"
+        });
+
+        return sendSuccess(res, STATUS_CODES.OK, MESSAGES.AUTH.LOG_OUT);
+    }
+    catch(err){
+        next(err);
+    }
+})
+
+// Get logged-in user
+app.get("/me", authenticate, async(req, res, next) => {
+    try{
+        const user = await UserModel.findById(req.user.id);
+        if(!user)
+            return sendError(res, STATUS_CODES.NOT_FOUND, MESSAGES.USER.NOT_FOUND);
+
+        return sendSuccess(res, STATUS_CODES.OK, MESSAGES.AUTH.LOGIN_SUCCESS, sanitizeUser(user));
+    }
+    catch(err){
+        next(err);
+    }
+})
+
 const adminRoutes = require('./routes/adminRoutes');
 // http://localhost:8000/admin
-app.use("/admin", adminRoutes);
+app.use("/admin", authenticate, authorize("admin"), adminRoutes);
 
 const userRoutes = require('./routes/userRoutes');
-// http://localhost:8000/admin
-app.use("/user", userRoutes);
+// http://localhost:8000/user
+app.use("/user", authenticate, authorize("user"), userRoutes);
 
 // http://localhost:8000
 // Handler function
@@ -87,13 +153,16 @@ app.post("/",(req, res)=>{
 app.put("/",(req, res, next)=>{
     const err = new Error("Something Went Wrong.....");
     return next(err);
-    res.send("Response from Put....");
+    // Removed dead code: res.send("Response from Put....") was unreachable
 })
 
 app.get("/emp", (req, res)=>{
-    userData = { userName: "Sachin", userAge : 45, userProfession : "Cricket"};
+    const userData = { userName: "Sachin", userAge : 45, userProfession : "Cricket"};   // Fixed: added `const`
     res.json(userData);     //to send data in object format - res.json()
 })
+
+// ------------ Error Handling Middleware (used at last, before listen) ---------------
+app.use(errorHandler);
 
 // listens to the request on the server
 app.listen(PORT, HOST, (error)=>{
@@ -102,11 +171,3 @@ app.listen(PORT, HOST, (error)=>{
     else
         console.log(`Server Running at http://${HOST}:${PORT}`);       
 })      
-
-// ------------ Error Handling Middleware (used at last)---------------
-const errorHandler = require("./middlewares/errorMiddleware");
-const UserModel = require("./models/userModel");
-const { sendError, sendSuccess } = require("./utils/responseHelpers");
-const STATUS_CODES = require("./constants/statusCodes");
-const MESSAGES = require("./constants/messages");
-app.use(errorHandler);
